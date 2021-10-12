452090fe679fabafecbd039b21aaeb8704aef060
Brian <brian.pcchang@gmail.com>
2021-10-11 18:52:54 -0700

    Complete Payment unit test

---
+++ src/main/java/com/amigoscode/testing/payment/Currency.java
@@ -0,0 +1,11 @@
+package com.amigoscode.testing.payment;
+
+public enum Currency {
+    CAD,
+    USD,
+    GBP,
+    EUR,
+    CNY,
+    TWD,
+    JPY
+}
--- src/test/java/com/amigoscode/testing/customer/CustomerRegistrationServiceTest.java
+++ src/test/java/com/amigoscode/testing/customer/CustomerRegistrationServiceTest.java
@@ -2,18 +2,19 @@ package com.amigoscode.testing.customer;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.*;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 
 class CustomerRegistrationServiceTest {
